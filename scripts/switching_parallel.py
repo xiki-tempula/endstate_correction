c62f76cc6f37e2047906c69397b18c40c694a092
saratk1 <sara.tkaczyk@gmail.com>
2022-05-31 13:47:25 +0200

    adjust switching script for openff and charmmff

--- scripts/switching_parallel.py
+++ scripts/switching_parallel.py
@@ -6,8 +6,13 @@ from os import path
 import numpy as np
 import torch
 from endstate_rew.neq import perform_switching
-from endstate_rew.system import generate_molecule, initialize_simulation
+from endstate_rew.system import (
+    generate_molecule,
+    initialize_simulation_with_charmmff,
+    initialize_simulation_with_openff,
+)
 from endstate_rew.constant import zinc_systems
+from glob import glob
 
 num_threads = 2
 torch.set_num_threads(num_threads)
@@ -24,62 +29,81 @@ elif len(sys.argv) == 2:
     smiles = "ClCCOCCCl"
     run_id = sys.argv[1]
 else:
-    raise RuntimeError()
+    raise RuntimeError("Run_id needs to be provided")
 
-print(f"{name=}")
-print(f"{smiles=}")
-print(f"{run_id=}")
-
-
+# choose ff
+ff = "charmmff"  # openff
+w_dir = f"/data/shared/projects/endstate_rew/{name}/"
 # equilibrium samples
 n_samples = 5_000
-n_steps_per_sample = 2_000
+n_steps_per_sample = 1_000
 #############
 # NEQ
 switching_length = 5_001
 nr_of_switches = 10
 #############
-w_dir = f"/data/shared/projects/endstate_rew/{name}/"
-run = "run01"
 
-mm_to_qml_filename = f"{w_dir}/switching/{run}/{name}_neq_ws_from_mm_to_qml_{nr_of_switches}_{switching_length}_{run_id}.pickle"
-qml_to_mm_filename = f"{w_dir}/switching/{run}/{name}_neq_ws_from_qml_to_mm_{nr_of_switches}_{switching_length}_{run_id}.pickle"
+print(f"{ff=}")
+print(f"{name=}")
+print(f"{smiles=}")
+print(f"{run_id=}")
+
+mm_to_qml_filename = f"{w_dir}/switching_{ff}/{name}_neq_ws_from_mm_to_qml_{nr_of_switches}_{switching_length}_{run_id}.pickle"
+qml_to_mm_filename = f"{w_dir}/switching_{ff}/{name}_neq_ws_from_qml_to_mm_{nr_of_switches}_{switching_length}_{run_id}.pickle"
 
 if path.isfile(mm_to_qml_filename) and path.isfile(qml_to_mm_filename):
     sys.exit()
 
+# create folder
+os.makedirs(f"{w_dir}/switching_{ff}", exist_ok=True)
+print(f"Generate directory: {w_dir}/switching_{ff}")
+
 ###########################################################################################
 ###########################################################################################
 # generate mol
-molecule = generate_molecule(smiles)
-sim = initialize_simulation(molecule, w_dir=w_dir)
+if ff == "openff" and smiles:
+    molecule = generate_molecule(forcefield=ff, smiles=smiles)
+elif ff == "charmmff" and smiles and not name:
+    raise RuntimeError("Charmff can not be used with SMILES input")
+else:
+    molecule = generate_molecule(forcefield=ff, name=name, base="../data/hipen_data")
+
+# initialize simulation depending on ff keyword
+if ff == "openff":
+    sim = initialize_simulation_with_openff(
+        molecule=molecule, w_dir=f"/data/shared/projects/endstate_rew/{name}/"
+    )
+elif ff == "charmmff":
+    sim = initialize_simulation_with_charmmff(molecule=molecule, zinc_id=name)
 ###########################################################################################
-# create folder
-os.makedirs(f"{w_dir}/switching/{run}", exist_ok=True)
-print(f"Generate directory: {w_dir}/switching/{run}")
 
 # load samples
-mm_samples = pickle.load(
-    open(
-        f"{w_dir}/sampling/{run}/{name}_mm_samples_{n_samples}_{n_steps_per_sample}.pickle",
-        "rb",
-    )
+mm_samples = []
+mm_sample_files = glob(
+    f"/data/shared/projects/endstate_rew/{name}/sampling_{ff}/run*/{name}_samples_{n_samples}_steps_{n_steps_per_sample}_lamb_0.0000.pickle"
 )
-qml_samples = pickle.load(
-    open(
-        f"{w_dir}/sampling/{run}/{name}_qml_samples_{n_samples}_{n_steps_per_sample}.pickle",
-        "rb",
-    )
+for samples in mm_sample_files:
+    mm_samples.append(pickle.load(open(samples, "rb")))
+
+qml_samples = []
+qml_sample_files = glob(
+    f"/data/shared/projects/endstate_rew/{name}/sampling_{ff}/run*/{name}_samples_{n_samples}_steps_{n_steps_per_sample}_lamb_1.0000.pickle"
 )
+for samples in qml_sample_files:
+    qml_samples.append(pickle.load(open(samples, "rb")))
 
 # perform switching only if file does not already exist
 if not path.isfile(mm_to_qml_filename):
     # define lambda space
     lambs = np.linspace(0, 1, switching_length)
     # perform NEQ from MM to QML
-    ws_from_mm_to_qml = perform_switching(
-        sim, lambdas=lambs, samples=mm_samples, nr_of_switches=nr_of_switches
-    )
+    ws_from_mm_to_qml = []
+    for sample in mm_samples:
+        ws_from_mm_to_qml.append(
+            perform_switching(
+                sim, lambdas=lambs, samples=sample, nr_of_switches=nr_of_switches
+            )
+        )
     # dump work values
     pickle.dump(ws_from_mm_to_qml, open(mm_to_qml_filename, "wb+"))
 
@@ -88,8 +112,12 @@ if not path.isfile(qml_to_mm_filename):
     # define lambda space
     lambs = np.linspace(1, 0, switching_length)
     # perform NEQ from QML to MM
-    ws_from_qml_to_mm = perform_switching(
-        sim, lambdas=lambs, samples=qml_samples, nr_of_switches=nr_of_switches
-    )
+    ws_from_qml_to_mm = []
+    for sample in qml_samples:
+        ws_from_qml_to_mm.append(
+            perform_switching(
+                sim, lambdas=lambs, samples=qml_samples, nr_of_switches=nr_of_switches
+            )
+        )
     # dump work values
     pickle.dump(ws_from_qml_to_mm, open(qml_to_mm_filename, "wb+"))
