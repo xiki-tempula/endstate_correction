9a430e3a14fa099a658a05d66aba5a4d4b990728
wiederm <marcus.wieder@univie.ac.at>
2022-06-27 16:45:08 +0200

    updating tests

--- endstate_rew/tests/test_neq.py
+++ endstate_rew/tests/test_neq.py
@@ -150,27 +150,27 @@ def test_switching(ff, dW_for, dW_rev):
     # dU_rev = dU(x)_mm - dU(x)_qml
     lambs = np.linspace(0, 1, 2)
     print(lambs)
-    dE_list = perform_switching(
+    dE_list, _ = perform_switching(
         sim, lambdas=lambs, samples=samples_mm[:1], nr_of_switches=1
     )
     assert np.isclose(dE_list[0].value_in_unit(unit.kilojoule_per_mole), dW_for)
     lambs = np.linspace(1, 0, 2)
     print(lambs)
-    dE_list = perform_switching(
+    dE_list, _ = perform_switching(
         sim, lambdas=lambs, samples=samples_mm[:1], nr_of_switches=1
     )
     assert np.isclose(dE_list[0].value_in_unit(unit.kilojoule_per_mole), dW_rev)
 
     # perform NEQ switching
     lambs = np.linspace(0, 1, 21)
-    dW_forw = perform_switching(
+    dW_forw, _ = perform_switching(
         sim, lambdas=lambs, samples=samples_mm[:1], nr_of_switches=1
     )
     print(dW_forw)
 
     # perform NEQ switching
     lambs = np.linspace(0, 1, 101)
-    dW_forw = perform_switching(
+    dW_forw, _ = perform_switching(
         sim, lambdas=lambs, samples=samples_mm[:1], nr_of_switches=1
     )
     print(dW_forw)
