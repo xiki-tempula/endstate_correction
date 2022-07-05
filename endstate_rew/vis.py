3a2f8f3a409ac7a5bd7bb26752cdf70e3a7bef6c
saratk1 <sara.tkaczyk@gmail.com>
2022-07-05 15:40:01 +0200

    adapted traj visualization

--- endstate_rew/vis.py
+++ endstate_rew/vis.py
@@ -1,32 +1,42 @@
 import pickle
 import nglview as ng
 from endstate_rew.system import generate_molecule
+from endstate_rew.constant import zinc_systems
 import mdtraj as md
 
 
-def visualize_mol(smiles: str, forcefield: str, pickle_file: str):
-    """Takes a smiles string and the path to a trajectory pickle file and returns
-    a nglview instance to visualize the MD simulation.
+def visualize_mol(
+    zinc_id: str,
+    forcefield: str,
+    endstate: str,
+    run_id: str = "",
+    w_dir: str = "/data/shared/projects/endstate_rew",
+    switching: bool = False,
+    switching_length: int = 5001,
+):
 
-    Args:
-        smiles (str): smiles sting defining the molecule
-        pickle_file (str): file path to pickled trajectorie
-
-    Returns:
-        _type_: nglview view instance
-    """
-    # generate mol from file
-    m = generate_molecule(forcefield=forcefield, smiles=smiles)
+    # get name
+    name, _ = zinc_systems[zinc_id]
+    # generate molecule object
+    m = generate_molecule(name=name, forcefield=forcefield)
     # write mol as pdb
     m.to_file("m.pdb", file_format="pdb")
+    # get pickle file
+    if not switching:
+        # get correct file label
+        if endstate == "mm":
+            lamb_nr = "0.0000"
+        elif endstate == "qml":
+            lamb_nr = "1.0000"
+        pickle_file = f"{w_dir}/{name}/sampling_{forcefield}/run{run_id}/{name}_samples_5000_steps_1000_lamb_{lamb_nr}.pickle"
+    else:
+        pickle_file = f"{w_dir}/{name}/switching_{forcefield}/{name}_samples_5000_steps_1000_lamb_{endstate}_endstate_nr_samples_500_switching_length_{switching_length}.pickle"
     # load traj
     f = pickle.load(open(pickle_file, "rb"))
     # load topology from pdb file
-    top = md.load("m.pdb").topology
-    # NOTE: the reason why this function needs a smiles string is because it
-    # has to generate a pdb file from which mdtraj reads the topology
+    # NOTE: pdb file is needed for mdtraj, which reads the topology
     # this is not very elegant # FIXME: try to load topology directly
-
+    top = md.load("m.pdb").topology
     # generate trajectory instance
     traj = md.Trajectory(f, topology=top)
     # align traj
