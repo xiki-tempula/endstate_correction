89b00450152be3609a394ddfa876bac5d2ca9a34
wiederm <marcus.wieder@univie.ac.at>
2022-08-23 10:07:40 +0200

    small changes in names

--- endstate_correction/utils.py
+++ endstate_correction/utils.py
@@ -3,21 +3,20 @@ import mdtraj as md
 from openmm import unit
 from openmm.app import CharmmPsfFile, CharmmCrdFile, PDBFile
 
-#def convert_pickle_to_
 
 def convert_pickle_to_dcd_file(
     pickle_file_path: str,
-    path_to_topology: str,
-    path_to_coords: str,
+    path_to_psf: str,
+    path_to_crd: str,
     dcd_output_path: str,
     pdb_output_path: str,
 ):
     # helper function that converts pickle trajectory file to dcd file
-    
+
     f = pickle.load(open(pickle_file_path, "rb"))
     traj = [frame.value_in_unit(unit.nanometer) for frame in f]
-    topology = CharmmPsfFile(path_to_topology).topology
-    positions = CharmmCrdFile(path_to_coords)
+    topology = CharmmPsfFile(path_to_psf).topology
+    positions = CharmmCrdFile(path_to_crd)
 
     PDBFile.writeFile(topology, positions.positions, file=open(pdb_output_path, "w"))
     traj = md.Trajectory(traj, topology=topology)
